b6934907ea87718b0fdf7a58f8161ef42b6a09e2
yumi-0223 <chym22048@g.nihon-u.ac.jp>
2024-01-10 13:17:11 +0900

    kiso

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+export default function App() {
+
+    
+
+    return (
+      <div>
+        <div>
+          <header>Apple New Member</header>
+        </div>
+        <main>
+          <h>
+            <h1>
+              <p>こんにちは。</p>
+              <p>日大テニスサークルです。</p>
+              <p>我々は主に他の学校のサークルなどと試合の際出場するメンバーです。</p>
+              <p>このサイトでは、女子レギュラーの概要を載せたものになります。</p>
+              <p>活動場所は、左に記載してある日本大学文理学部の100周年記念館のテニスコートになります。</p>
+              <p>週に3日3時間ほど練習をしています。</p>
+              <p>興味を持っていただけましたら、<a href="https://forms.gle/ChnRMwGqM4bBY92a9">link</a>に連絡お願いします。</p>
+            </h1>
+            <h2>
+              日本大学文理学部情報科学科Webプログラミング
+            </h2>
+          </h>
+        </main>
+      </div>
+    );
+  }
